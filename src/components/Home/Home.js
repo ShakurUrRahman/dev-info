8d7d5f28ee056c910938e2057116887f45f42b42
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2022-10-27 23:13:42 +0600

    readme file and blog

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             <div className="hero-content text-center text-neutral-content">
                 <div className="flex flex-col ">
                     <h1 className="mb-5 text-5xl font-bold lg:mt-16 mt-8 text-[#c2b2de]">Now this is the time to prove yourself!!</h1>
-                    <p className="mb-5 text-[#F7E018] lg:mx-24 mx-3">We made for you some topics wise quizzes. You can go through it and sharp your knowledge by this. Please choose your section and by clicking the start button.</p>
+                    <p className="mb-5 text-[#F7E018] lg:mx-24 mx-3">We made for you some topics wise quizzes. You can go through it and sharp your knowledge by this. Please choose your section by clicking the START QUIZ button.</p>
                     <div className='lg:grid grid-cols-2 lg:gap-10 lg:mt-12 mx-auto'>
                         {
                             topics.map(topic => <Topic
